5fda61f36780975c4a111135036c2a6a78096b3a
unknown <wafaazek@gmail.com>
2023-12-12 16:39:12 +0100

    finalproduct

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,28 +3,26 @@ module.exports = {
   content: ["./src/*"],
   theme: {
     extend: {
-     
-
-
-      colors:{
-
-        navColor:"#314584",
-        btnColor:"#ff3946",
-        pColor:"#6e757a"
+      colors: {
+        navColor: "#314584",
+        btnColor: "#ff3946",
+        pColor: "#6e757a",
+        cardColor: "#ff3946",
       },
-      fontSize:{
-
-        navSize:"24px",
-        titleSize:"74px",
-        pSize:"18px"
+      fontSize: {
+        navSize: "24px",
+        titleSize: "74px",
+        pSize: "18px",
+        SecTitleSize: "45px",
+        section2Size: "28px",
+        pSSize: "15px",
+        footerTitleSize: "49px",
       },
 
-      fontFamily:{
-
-        fontNav:["Work Sans"]
-      }
+      fontFamily: {
+        fontNav: ["Work Sans"],
+      },
     },
   },
   plugins: [],
-}
-
+};
